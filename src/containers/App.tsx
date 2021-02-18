c0d419073fb6a3ce541d70dc2ee30974c5877a84
F.B.I <shorshalka.home@gmail.com>
2021-02-19 01:37:35 +0200

    initial commit

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,7 +1,57 @@
 import React from "react"
+import Navigation from "../components/Navigation/Navigation"
 
-function App() {
-  return <h1> App </h1>
+const curentD: Date = new Date()
+
+interface IAppProps {
+  date?: Date
+}
+
+interface IAppState {
+  currentDate: Date
+}
+
+class App extends React.Component<IAppProps, IAppState> {
+  public static defaultProps = {
+    d: new Date(),
+  }
+
+  constructor(props: IAppProps) {
+    super(props)
+
+    this.state = {
+      currentDate: curentD,
+    }
+  }
+
+  setNextMonth = () => {
+    this.setState((prevState) => {
+      console.log(prevState)
+      const nextState = {
+        currentDate: new Date(prevState.currentDate.setMonth(prevState.currentDate.getMonth() + 1)),
+      }
+      return nextState
+    })
+  }
+
+  setPrevMonth = () => {
+    this.setState((prevState) => {
+      console.log(prevState)
+      const nextState = {
+        currentDate: new Date(prevState.currentDate.setMonth(prevState.currentDate.getMonth() - 1)),
+      }
+      return nextState
+    })
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>App TSX</h1>
+        <Navigation date={this.state.currentDate} setNextMonth={this.setNextMonth} setPrevMonth={this.setPrevMonth} />
+      </React.Fragment>
+    )
+  }
 }
 
 export default App
