5fefb4ecf8a01be62b0a859eab4750c3c73b391c
LaughingThroll <laughingthroll@gmail.com>
2021-02-20 02:14:49 +0200

    added: calendar-header

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,31 +1,43 @@
 import React from "react"
 
 import Navigation from "../components/Navigation"
+import CalendarHeader from "../components/CalendarHeader/CalendarHeader"
+
+import { daysInMonth } from "../utils/date"
 
 interface IAppState {
   currentDate: Date
+  daysInMonth: number
 }
 
 class App extends React.Component {
   state: IAppState = {
     currentDate: new Date(),
+    daysInMonth: daysInMonth(new Date()),
   }
 
   setNextMonth = () => {
     const currentDate = new Date(this.state.currentDate.setMonth(this.state.currentDate.getMonth() + 1))
-    this.setState({ currentDate })
+    this.setState({ currentDate, daysInMonth: daysInMonth(currentDate) })
   }
 
   setPrevMonth = () => {
     const currentDate = new Date(this.state.currentDate.setMonth(this.state.currentDate.getMonth() - 1))
-    this.setState({ currentDate })
+    this.setState({ currentDate, daysInMonth: daysInMonth(currentDate) })
   }
 
   render() {
+    const { currentDate, daysInMonth } = this.state
+
     return (
-      <React.Fragment>
-        <Navigation date={this.state.currentDate} setNextMonth={this.setNextMonth} setPrevMonth={this.setPrevMonth} />
-      </React.Fragment>
+      <div className="container">
+        <Navigation date={currentDate} setNextMonth={this.setNextMonth} setPrevMonth={this.setPrevMonth} />
+        <table className="calendar-table">
+          <CalendarHeader date={currentDate} daysInMonth={daysInMonth} />
+
+          <tbody></tbody>
+        </table>
+      </div>
     )
   }
 }
